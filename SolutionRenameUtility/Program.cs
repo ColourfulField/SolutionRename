32b31ddafc75372633ddf33de508bef30ef06283
Mike Shevchik <shevchikmike@gmail.com>
2017-11-26 13:34:03 +0200

    First version of the app

---
+++ SolutionRenameUtility/Program.cs
@@ -0,0 +1,139 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SolutionRenameUtility
+{
+    class Program
+    {
+        private static readonly string _folderSeparator = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\\" : "/";
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            #region Argument checks 
+
+            if (args.Length < 2)
+            {
+                PrintUsage();
+            }
+            
+            if (args.Length == 3)
+            {
+                path = args[2];
+                if (!Directory.Exists(path))
+                {
+                    PrintUsage();
+                    return;
+                }
+            }
+
+            var oldSolutionName = args[0];
+            var newSolutionName = args[1];
+            if (oldSolutionName.Length <= 3 || newSolutionName.Length <= 3)
+            {
+                Console.WriteLine("Please specify longer solution names. Current name is unsafe to rename as it may accidentally lead to undesired renames.");
+            }
+
+            #endregion
+
+            PrintWarningMessage(oldSolutionName, newSolutionName, path);
+
+            var key = Console.ReadKey();
+            if (key.Key == ConsoleKey.Y)
+            {
+                //CreateBackup(path);
+                RenameSolution(oldSolutionName, newSolutionName, path);
+                Console.WriteLine("Solution renamed successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Aborting...");
+            }
+            
+        }
+
+        private static void RenameSolution(string oldSolutionName, string newSolutionName, string path)
+        {
+            var filePaths = Directory.GetFiles(path);
+
+            foreach (var filePath in filePaths)
+            {
+                ModifyFile(filePath, oldSolutionName, newSolutionName);
+            }
+
+            var folderPaths = Directory.GetDirectories(path);
+
+            foreach (var folderPath in folderPaths)
+            {
+                string renamedFolderPath = Rename(folderPath, oldSolutionName, newSolutionName);
+                RenameSolution(oldSolutionName, newSolutionName, renamedFolderPath);
+            }
+
+        }
+
+        private static void ModifyFile(string filePath, string oldSolutionName, string newSolutionName)
+        {
+            var newFilePath = Rename(filePath, oldSolutionName, newSolutionName);
+
+            var fileText = File.ReadAllText(newFilePath);
+            var modifiedFileText = fileText.Replace(oldSolutionName, newSolutionName);
+            if (modifiedFileText != fileText && !filePath.EndsWith(".dll") && !filePath.EndsWith(".exe"))
+            {
+                File.WriteAllText(newFilePath, modifiedFileText);
+            }
+        }
+
+        private static string Rename(string path, string oldName, string newName)
+        {
+            // Reassemble folder path with new name
+            var pathParts = path.Split(_folderSeparator);
+            StringBuilder newPath = new StringBuilder();
+            foreach (var pathPart in pathParts.Take(pathParts.Length - 1))
+            {
+                newPath.Append($"{pathPart}{_folderSeparator}");
+            }
+
+            newPath.Append(pathParts.Last().Replace(oldName, newName));
+
+            if (newPath.ToString() != path)
+            {
+                if (Directory.Exists(path))
+                {
+                    Directory.Move(path, newPath.ToString());
+                }
+                else
+                { 
+                    File.Move(path, newPath.ToString());
+                }
+            }
+
+            return newPath.ToString();
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Incorrent argument list. Usage: \"SolutionRenameUtility OldSolutionName NewSolutionName Path \"." +
+                              "The path parameter is optional if you launch this utility from a root solution folder.");
+        }
+
+        private static void PrintWarningMessage(string oldSolutionName, string newSolutionName, string path)
+        {
+            Console.Write("Are you sure you want to rename ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(oldSolutionName);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" to ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(newSolutionName);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" in ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(path + "?\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Y/N");
+        }
+    }
+}
